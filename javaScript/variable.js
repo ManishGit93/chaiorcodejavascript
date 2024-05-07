90d97b5fbef5115dbae5d0084c0ca74929a95a7d
Manish <manish93web@gmail.com>
2024-05-08 01:00:12 +0530

    variables

---
+++ javaScript/variable.js
@@ -0,0 +1,15 @@
+const accountId = 6876868;
+let accountmail = "jghjghj";
+var accountpassword = "hjghj";
+accountCity = "dkd"
+
+// accountId = 2;
+
+accountmail = "jkjk"
+accountpassword = "663"
+
+console.log(accountId);
+console.table([accountId,accountmail,accountpassword,accountCity])
+
+
+
