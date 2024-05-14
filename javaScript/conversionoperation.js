7e0fd131e2b0444957b31705d4825b8161f6e19c
Manish <manish93web@gmail.com>
2024-05-14 23:59:22 +0530

    conversion

---
+++ javaScript/conversionoperation.js
@@ -0,0 +1,30 @@
+let score = "Manish";
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn =  Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2*2);
+
+
+
+
