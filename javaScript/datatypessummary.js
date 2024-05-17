a1de9422a1037fb734d4e9408d04e86b07d02ff2
Manish <manish93web@gmail.com>
2024-05-18 00:20:46 +0530

    jjaj

---
+++ javaScript/datatypessummary.js
@@ -0,0 +1,19 @@
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn = false;
+const outsideTemp = null
+let userEmail;
+const id =  Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+const heros = ["nkk","hjh","hkhk"];
+
+let myObj = {
+    name: "jajh",
+    age:22
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
