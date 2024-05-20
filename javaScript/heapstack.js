76eb2ecb2bce38c0b2e698d15596f0f483f5b52e
Manish <manish93web@gmail.com>
2024-05-20 23:38:20 +0530

    heapstak

---
+++ javaScript/heapstack.js
@@ -0,0 +1,24 @@
+let myYoutubename = "FrontendHome";
+
+let anotherName = myYoutubename;
+anotherName = "SanchitCoder";
+
+console.log(anotherName);
+console.log(myYoutubename);
+
+let userOne = {
+    email:"user@google.com",
+    upi:"user@yabl"
+}
+
+let userTwo = userOne;
+
+userTwo.email
+
+console.log(userTwo);
+console.log(userOne);
+
+// let  userTwo = {
+//     email :"hhkka@gmail.com",
+//     upi:"jjaj@ybl"
+// }
