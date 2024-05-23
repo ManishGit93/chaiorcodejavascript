d66258f740fad7a401e21ddbd0db7b2fd791433f
Manish <manish93web@gmail.com>
2024-05-23 22:58:52 +0530

    maths_nums

--- javaScript/nums_maths.js
+++ javaScript/nums_maths.js
@@ -14,3 +14,15 @@ const hundreds = 100000;
 console.log(hundreds);
 
 console.log(hundreds.toLocaleString('en-IN'))
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4.9));
+console.log(Math.random());
+console.log(Math.random()*10)
+
+const min = 10;
+const max = 20;
