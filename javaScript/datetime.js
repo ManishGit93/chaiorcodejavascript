175a704ff405bea6e13329bea5f9ed931f596b5c
Manish <manish93web@gmail.com>
2024-05-23 23:29:38 +0530

    date_time

---
+++ javaScript/datetime.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString());
+console.log(typeof myDate)
+let myCreateDate = new Date();
+console.log(myCreateDate);
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreateDate.getMonth())
+console.log(myCreateDate.getDay())
