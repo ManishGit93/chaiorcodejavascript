2b2be00563ec3ecf06e25a261c9c261923c9de8d
Manish <manish93web@gmail.com>
2024-06-02 23:21:56 +0530

    object example

---
+++ javaScript/basicjs/object2.js
@@ -0,0 +1,17 @@
+const course = {
+    cousreName: "js in Hindi",
+    price:"999",
+    courseInstructor:"Manish"
+}
+
+// course.courseInstructor
+
+const {courseInstructor:instructor} = course
+
+console.log(instructor);
+
+const navbar = ()=>{
+
+}
+
+navbar(company = "Manish Company");
