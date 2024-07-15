1838095bbdc295c44edf9902f84062fa9e14f841
Manish <manish93web@gmail.com>
2024-07-16 00:49:10 +0530

    basic

---
+++ javaScript/basicjs/23equalcheck.js
@@ -0,0 +1,12 @@
+let x= 5;
+let y = 4;
+
+if(x==y){
+    console.log("Double Equal: executed")
+}
+
+if(x===y){
+    console.log("Triple equal exequated")
+} else if(x !=y){
+    console.log("this executed")
+}
---
+++ javaScript/basicjs/let.js
@@ -0,0 +1,11 @@
+function myLetexample(){
+    if(true){
+        let count= 10;
+        console.log(count);
+    }
+
+    console.log(count);
+
+}
+
+myLetexample();
