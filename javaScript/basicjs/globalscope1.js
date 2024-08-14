f5c7f16e0bf7e4296dc8051342104311c59b7084
Manish <manish93web@gmail.com>
2024-08-14 15:57:20 +0530

    globalscope

---
+++ javaScript/basicjs/globalscope1.js
@@ -0,0 +1,34 @@
+function one() {
+    const userName = "Manish";
+    // console.log("FAFA")
+
+    function two() {
+        const website = "youtube";
+        // console.log("oneUserName:",userName)
+    }
+    // console.log("Two:",website);
+
+    // two()
+}
+
+// one()
+
+
+if(true){
+    const userName = "Manish"
+    if(userName === "Manish") {
+        const website = " youtube"
+        // console.log(userName + website)
+    }
+    // console.log(website)
+}
+
+// console.log(userName)
+
+addOne(7)
+function addOne (num){
+    num = num + 1
+    console.log(num);
+    return num;
+}
+
