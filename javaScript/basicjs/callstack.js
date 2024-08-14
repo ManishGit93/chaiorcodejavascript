dde06b2c5fbea7295cb73868c23236fa287ea94d
Manish <manish93web@gmail.com>
2024-08-14 17:15:30 +0530

    call stack example

---
+++ javaScript/basicjs/callstack.js
@@ -0,0 +1,9 @@
+let val1 = 10;
+let val2 =9;
+function addNumber(num1,num2){
+    let total =  num1 + num2;
+    console.log("Total of That Number:",total)
+    return total
+}
+let result1 = addNumber(val1,val2)
+let result2 = addNumber(10,2) 
